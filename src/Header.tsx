cf50432f9b702f64d8d61bdcf169ab8b8abe1b4e
Bogdan-design <bogdanbw@gmail.com>
2022-12-10 15:10:27 +0100

    change README.md

---
+++ src/Header.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const Header = () => {
+    return (
+        <div>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+    )
+}
---
+++ src/Technologies.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const Technologies = () => {
+    return (
+        <div>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+    )
+}
